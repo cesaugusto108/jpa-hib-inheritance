66f0d8e1af17315fd1a645ed19cbd04f6c1519d6
Cesar Augusto <ces.augusto108@gmail.com>
2023-05-11 20:28:49 -0300

    Initial project setup

---
+++ adv-hibernate/src/main/java/augusto108/ces/advhibernate/Main.java
@@ -0,0 +1,11 @@
+package augusto108.ces.advhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
