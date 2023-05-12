5b249a1acea3c3b84ad81ce27e778651bb5230b5
César Augusto <ces.augusto108@gmail.com>
2023-05-12 10:08:31 -0300

    Employee database load

--- adv-hibernate/src/main/java/augusto108/ces/advhibernate/config/DatabasePreLoad.java
+++ adv-hibernate/src/main/java/augusto108/ces/advhibernate/config/DatabasePreLoad.java
@@ -1,7 +1,9 @@
 package augusto108.ces.advhibernate.config;
 
+import augusto108.ces.advhibernate.data.EmployeeLoad;
 import augusto108.ces.advhibernate.data.InstructorLoad;
 import augusto108.ces.advhibernate.data.StudentLoad;
+import augusto108.ces.advhibernate.domain.entities.Employee;
 import augusto108.ces.advhibernate.domain.entities.Instructor;
 import augusto108.ces.advhibernate.domain.entities.Student;
 import augusto108.ces.advhibernate.domain.entities.Telephone;
@@ -11,22 +13,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DatabaseSamplesLoad {
+public class DatabasePreLoad {
     private final PersonService personService;
     private final TelephoneService telephoneService;
     private final StudentLoad studentLoad;
     private final InstructorLoad instructorLoad;
+    private final EmployeeLoad employeeLoad;
 
-    public DatabaseSamplesLoad(
+    public DatabasePreLoad(
             PersonService personService,
             TelephoneService telephoneService,
             StudentLoad studentLoad,
-            InstructorLoad instructorLoad
-    ) {
+            InstructorLoad instructorLoad,
+            EmployeeLoad employeeLoad) {
         this.personService = personService;
         this.telephoneService = telephoneService;
         this.studentLoad = studentLoad;
         this.instructorLoad = instructorLoad;
+        this.employeeLoad = employeeLoad;
     }
 
     @Bean
@@ -51,5 +55,17 @@ public class DatabaseSamplesLoad {
         }
 
         personService.persistPerson(instructor);
+    }
+
+    @Bean
+    void persistEmployee() {
+        final Employee employee = employeeLoad.createEmployee();
+
+        for (Telephone telephone : employeeLoad.createTelephones()) {
+            telephoneService.persistTelephone(telephone);
+            employee.getTelephones().add(telephone);
+        }
+
+        personService.persistPerson(employee);
     }
 }
---
+++ adv-hibernate/src/main/java/augusto108/ces/advhibernate/data/EmployeeLoad.java
@@ -0,0 +1,53 @@
+package augusto108.ces.advhibernate.data;
+
+import augusto108.ces.advhibernate.domain.entities.Employee;
+import augusto108.ces.advhibernate.domain.entities.Name;
+import augusto108.ces.advhibernate.domain.entities.Telephone;
+import augusto108.ces.advhibernate.domain.entities.enums.Role;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:sample-employee.properties")
+public class EmployeeLoad {
+    @Value("${employee.first-name}")
+    private String firstName;
+
+    @Value("${employee.middle-name}")
+    private String middleName;
+
+    @Value("${employee.last-name}")
+    private String lastName;
+
+    @Value("${employee.email}")
+    private String email;
+
+    @Value("${employee.role}")
+    private Role role;
+
+    @Value("${employee.telephone.country-code}")
+    private String countryCode;
+
+    @Value("${employee.telephone.area-code}")
+    private String areaCode;
+
+    @Value("${employee.telephone1.number}")
+    private String firstPhoneNumber;
+
+    @Value("${employee.telephone2.number}")
+    private String secondPhoneNumber;
+
+    public Employee createEmployee() {
+        final Name name = new Name(firstName, middleName, lastName);
+
+        return new Employee(name, name, email, role);
+    }
+
+    public Telephone[] createTelephones() {
+        return new Telephone[]{
+                new Telephone(countryCode, areaCode, firstPhoneNumber),
+                new Telephone(countryCode, areaCode, secondPhoneNumber)
+        };
+    }
+}
