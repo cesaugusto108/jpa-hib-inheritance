abac549432a06ea45493df818041d5f8f2d3271c
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-16 23:39:54 -0300

    EmployeeControllerTest tests

---
+++ adv-hibernate/src/test/resources/employees-script.sql
@@ -0,0 +1,13 @@
+insert
+    into person (`id`, `email`, `first_name`, `last_name`, `middle_name`, `social_first_name`, `social_last_name`, `social_middle_name`)
+    values (3004, 'gabriela@email.com', 'Gabriela', 'Ferreira', 'Santos', 'Gabriela', 'Ferreira', 'Santos');
+
+insert
+    into employee (`employee_role`, `id`) values ('ADMIN', 3004);
+
+insert
+    into person (`id`, `email`, `first_name`, `last_name`, `middle_name`, `social_first_name`, `social_last_name`, `social_middle_name`)
+    values (3005, 'fernanda@email.com', 'Fernanda', 'Martins', 'Carvalho', 'Fernanda', 'Martins', 'Carvalho');
+
+insert
+    into employee (`employee_role`, `id`) values ('TRAINEE', 3005);
