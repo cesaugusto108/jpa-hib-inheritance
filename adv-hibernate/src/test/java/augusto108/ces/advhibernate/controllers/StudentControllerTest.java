0e56667e84c99eded3aeabea2e84c745e9a88243
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-16 21:59:18 -0300

    StudentControllerTest tests refactoring

--- adv-hibernate/src/test/java/augusto108/ces/advhibernate/controllers/StudentControllerTest.java
+++ adv-hibernate/src/test/java/augusto108/ces/advhibernate/controllers/StudentControllerTest.java
@@ -1,5 +1,7 @@
 package augusto108.ces.advhibernate.controllers;
 
+import augusto108.ces.advhibernate.domain.entities.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -13,8 +15,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,31 +36,44 @@ class StudentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private final String jsonContent = "{\"id\":3000,\"telephones\":[],\"name\":{\"firstName\":\"Larissa\",\"middleName\":\"Pereira\"," +
-            "\"lastName\":\"Castro\"},\"socialName\":{\"firstName\":\"Larissa\",\"middleName\":\"Pereira\",\"lastName\":\"Castro\"}," +
-            "\"email\":\"larissa@email.com\",\"registration\":\"202000011201\"}";
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Sql("/students-script.sql")
     @Test
     void getStudents() throws Exception {
-        final MvcResult result = mockMvc.perform(get("/students/all").contentType(MediaType.APPLICATION_JSON))
+        final MvcResult result = mockMvc.perform(
+                        get("/students/all").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(2))).andReturn();
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andReturn();
 
-        assertTrue(result.getResponse().getContentAsString().contains(jsonContent));
+        final String jsonContent = result.getResponse().getContentAsString();
+
+        final boolean containsId3000 = objectMapper
+                .readValue(jsonContent, ArrayList.class).get(0).toString().contains("id=3000");
+        final boolean containsId3001 = objectMapper
+                .readValue(jsonContent, ArrayList.class).get(1).toString().contains("id=3001");
+
+        assertTrue(containsId3000 && containsId3001);
     }
 
     @Sql("/students-script.sql")
     @Test
     void getStudentById() throws Exception {
-        final MvcResult result = mockMvc.perform(get("/students/{id}", 3000).contentType(MediaType.APPLICATION_JSON))
+        final MvcResult result = mockMvc.perform(
+                        get("/students/{id}", 3000).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(3000)))
                 .andExpect(jsonPath("$.telephones", hasSize(0)))
-                .andExpect(jsonPath("$.registration", is("202000011201"))).andReturn();
+                .andExpect(jsonPath("$.registration", is("202000011201")))
+                .andReturn();
 
-        assertTrue(result.getResponse().getContentAsString().contains(jsonContent));
+        final String jsonContent = result.getResponse().getContentAsString();
+        final Student student = objectMapper.readValue(jsonContent, Student.class);
+
+        assertEquals("Larissa Pereira Castro (202000011201)", student.toString());
     }
 }
