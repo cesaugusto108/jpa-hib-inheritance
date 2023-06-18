b59a90c0577490d41f7625d572c646eee1cd2ba4
Cesar Augusto <ces.augusto108@gmail.com>
2023-06-17 21:32:46 -0300

    Instructors database load for EmployeeControllerTest tests

---
+++ adv-hibernate/src/test/resources/instructors-script.sql
@@ -0,0 +1,8 @@
+insert
+    into person (`id`, `email`, `first_name`, `last_name`, `middle_name`, `social_first_name`, `social_last_name`, `social_middle_name`, `specialty`)
+    values (20002, 'luciana@email.com', 'Luciana', 'Santos', 'Silva', 'Luciana', 'Santos', 'Silva', 'Kotlin');
+
+
+insert
+    into person (`id`, `email`, `first_name`, `last_name`, `middle_name`, `social_first_name`, `social_last_name`, `social_middle_name`, `specialty`)
+    values (20003, 'simone@email.com', 'Ítalo', 'Pereira', 'Martins', 'Ítalo', 'Pereira', 'Martins', 'Clojure');
